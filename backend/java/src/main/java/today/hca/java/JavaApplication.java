62f45ed2e7e9f79092a69f1ab2ab231d675ca428
mjpark1329@naver.com <mjpark1329@naver.com>
2023-09-08 17:43:16 +0900

    :zap: feat: 유저가 단어 추측 시 유사도 반환하는 api 추가 중

--- backend/java/src/main/java/today/hca/java/JavaApplication.java
+++ backend/java/src/main/java/today/hca/java/JavaApplication.java
@@ -2,8 +2,9 @@ package today.hca.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class JavaApplication {
 
     public static void main(String[] args) {
