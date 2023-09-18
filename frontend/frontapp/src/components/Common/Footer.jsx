ff1346e844fa2f75e8a7c59cca982b2e8ea13a5f
itsdangerous <danger121@naver.com>
2023-09-18 22:40:15 +0900

    :truck: rename: Common 폴더로 이동

---
+++ frontend/frontapp/src/components/Common/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 200px;
+
+  width: 100%;
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin-top: 200px;
+`;
+
+const Message = styled.div`
+  position: relative;
+  color: #253846;
+  text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+  font-weight: bold;
+  font-size: 40px;
+  overflow: hidden;
+  padding: 5px 0;
+`;
+
+export default function Footer() {
+  return (
+    <Container>
+      <Message>이곳은 푸터입니다만 .ㅎ</Message>
+    </Container>
+  );
+}
