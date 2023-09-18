669e2f967da5d53304333b76b3ff64ff2a8e882a
itsdangerous <danger121@naver.com>
2023-09-18 22:42:14 +0900

    :pencil2: fix: import 경로 수정

--- frontend/frontapp/src/routes/QuizResult.jsx
+++ frontend/frontapp/src/routes/QuizResult.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../components/Common/Footer";
+import Header from "../components/Common/Header";
 import Main from "../components/Main/Main";
 import News from "../components/News/News";
 
