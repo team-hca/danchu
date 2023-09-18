2870a50c19139d4a380ffa43ec22e0e591ed76ce
itsdangerous <danger121@naver.com>
2023-09-18 17:53:49 +0900

    :sparkles: feat: 오늘의 퀴즈 부분 기능 구현

---
+++ frontend/frontapp/src/components/Main/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderBox from "./MainBox";
+import HeaderMessage from "./MainMessage";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+  width: 720px;
+`;
+
+export default function Main() {
+  return (
+    <Container>
+      <HeaderMessage />
+      <HeaderBox />
+    </Container>
+  );
+}
