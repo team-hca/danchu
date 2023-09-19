65a6f8986b039f87a0be2c78069ac796ce072d06
김유섭 <dbtjq1592000@gmail.com>
2023-09-19 12:04:16 +0900

    Revert "Merge branch 'frontend/feat/result-page' into 'master'"
    This reverts merge request !4

--- frontend/frontapp/src/index.js
+++ frontend/frontapp/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ResetStyle from './styles/reset';
-
-import { RouterProvider } from 'react-router-dom';
-import router from './Router';
-import GlobalVariableStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
-    <ResetStyle />
-    <RouterProvider router={router}/>
-    <GlobalVariableStyle></GlobalVariableStyle>
+    <App />
   </React.StrictMode>
 );
 
