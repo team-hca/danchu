5d6ae88ee575159b0b8086344f8be81149b6ec4c
이정민 <jmlee2705@gmail.com>
2023-09-21 17:33:08 +0900

    ⚡ feat: 성공 내용 변수로 설정

--- frontend/frontapp/src/components/CongratModal.jsx
+++ frontend/frontapp/src/components/CongratModal.jsx
@@ -6,6 +6,10 @@ const ModalContainer = styled.div`
   margin: 40px 0px 40px 0px;
 `
 
+const SuccessContent = styled.div`
+
+`
+
 const CongratTitle = styled.span`
   color: #FFF7D4;
   font-weight: bold;
@@ -40,6 +44,25 @@ const DanchuRank = styled.div`
   margin: 10px 0px 10px 0px;
 `
 
+const now = new Date();
+const options = { timeZone: 'Asia/Seoul' };
+const krSeoulTime = now.toLocaleString('en-US', options);
+
+const dateArray = krSeoulTime.split(', ')[0].split('/');
+
+const year = parseInt(dateArray[2], 10);
+let month = parseInt(dateArray[0], 10).toString().padStart(2, '0');
+let date = parseInt(dateArray[1], 10).toString().padStart(2, '0');
+
+const trialCnt = 999999;
+const streakCnt = 12341234;
+const rankPercentage = 1;
+
+const danchuDate = year + "년 " + month + "일 " + date + "일 단추"
+const danchuTrial = "시도 횟수: " + trialCnt
+const danchuStreak = "연속으로 맞은 횟수: " + streakCnt
+const danchuRank = "유저랭킹: 상위 " + rankPercentage + "%"
+
 export default function CongratModal() {
   return (
     <ModalContainer>
@@ -47,12 +70,12 @@ export default function CongratModal() {
         축하합니다! <br />
         오늘의 단추를 풀었습니다!
       </CongratTitle>
-      <div>
-        <DanchuDate>단추</DanchuDate>
-        <DanchuTrial>시도 횟수 : </DanchuTrial>
-        <DanchuStreak>연속으로 맞은 횟수 : </DanchuStreak>
-        <DanchuRank>유저 랭킹: 상위 </DanchuRank>
-      </div>
+      <SuccessContent>
+        <DanchuDate>{danchuDate}</DanchuDate>
+        <DanchuTrial>{danchuTrial}</DanchuTrial>
+        <DanchuStreak>{danchuStreak}</DanchuStreak>
+        <DanchuRank>{danchuRank}</DanchuRank>
+      </SuccessContent>
     </ModalContainer>
   );
 }
