453aa452c1ab710342d0bcf5f36eec5b8917b13c
이정민 <jmlee2705@gmail.com>
2023-09-21 15:13:28 +0900

    ✨ feat: 퀴즈 정답 모달 구현

---
+++ frontend/frontapp/src/components/CongratModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ModalContainer = styled.div`
+  background-color: #253846;
+  margin: 40px 0px 40px 0px;
+`
+
+export default function CongratModal() {
+  return (
+    <ModalContainer>
+    </ModalContainer>
+  );
+}
