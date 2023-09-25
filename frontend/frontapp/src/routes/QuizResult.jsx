edea12765e57f096557e1682f9739736a95c8224
itsdangerous <danger121@naver.com>
2023-09-25 14:55:07 +0900

    :sparkles: feat: result의 문제 부분 따로 구현

--- frontend/frontapp/src/routes/QuizResult.jsx
+++ frontend/frontapp/src/routes/QuizResult.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import axios from "axios";
+import { React, useEffect, useState } from "react";
 import styled from "styled-components";
 import Footer from "../components/Common/Footer";
 import Header from "../components/Common/Header";
-import Main from "../components/Main/Main";
+import Answer from "../components/News/Answer";
 import News from "../components/News/News";
 
 const Container = styled.div`
@@ -13,11 +14,12 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
 `;
+let today = new Date();
 
 // 로컬스토리지에 word배열 저장하는 함수 - 테스트 데이터
 function saveWordsToLocalStorage(data) {
   const formattedData = {
-    word1: data.word1 || null,
+    word1: data.word1,
     word2: data.word2 || null,
     word3: data.word3 || null,
   };
@@ -28,10 +30,26 @@ function getWordsFromLocalStorage() {
   const savedData = localStorage.getItem("words");
   return savedData
     ? JSON.parse(savedData)
-    : { words: { word1: null, word2: null, word3: null } };
+    : { word1: null, word2: null, word3: null };
 }
 
 export default function QuizResult() {
+  // QuizResult props로 sentence 받는 걸로 수정해야 함
+  const [quizSentence, setQuizSentence] = useState();
+  useEffect(() => {
+    console.log("찍는다~");
+    axios
+      .get(
+        `/api/v1/quiz/today?date=${today.getFullYear()}-${(today.getMonth() + 1)
+          .toString()
+          .padStart(2, "0")}-${today.getDate().toString().padStart(2, "0")}`
+      )
+      .then((response) => {
+        setQuizSentence(response.data);
+        console.log("QuizReulst에서 찍는 response.data : ", response.data);
+      });
+  }, []);
+
   // 로컬에 저장 테스트
   const wordsArray = { word1: "과자", word2: "아이스크림", word3: null };
   saveWordsToLocalStorage(wordsArray);
@@ -44,7 +62,8 @@ export default function QuizResult() {
       {/* <Navbar /> */}
       <Container>
         <Header />
-        <Main words={words} />
+        <Answer quizSentence={quizSentence} />
+
         <News words={words} />
       </Container>
       <Footer />
