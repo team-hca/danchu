12ec1adf6cdcf053c077bb895c1d0424a1ded290
itsdangerous <danger121@naver.com>
2023-09-25 14:52:57 +0900

    :sparkles: feat: result의 문제 부분 따로 구현

---
+++ frontend/frontapp/src/components/News/Answer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import AnswerBox from "./AnswerBox";
+import AnswerMessage from "./AnswerMessage";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+  width: 720px;
+  margin-bottom: 100px;
+`;
+
+export default function Answer(quizSentence) {
+  console.log("QuizResult에서 받은 quizSentence : ", quizSentence);
+  return (
+    <Container>
+      <AnswerMessage />
+      <AnswerBox quizSentence={quizSentence} />
+    </Container>
+  );
+}
