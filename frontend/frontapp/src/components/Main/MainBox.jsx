1dec8d486c5281b567a8f9844fbca9db7d07cc2d
itsdangerous <danger121@naver.com>
2023-09-27 14:12:33 +0900

    :hammer: fix: 예외상황 더 추가

--- frontend/frontapp/src/components/Main/MainBox.jsx
+++ frontend/frontapp/src/components/Main/MainBox.jsx
@@ -107,8 +107,9 @@ export default function MainBox(props) {
           if (idx === 0) return [...acc, part];
           let content =
             active !== undefined
-              ? localStorage.getItem(guesses[i]) !== null &&
-                JSON.parse(localStorage.getItem(guesses[i]))[0] !== null
+              ? localStorage.getItem(guesses[i]) &&
+                JSON.parse(localStorage.getItem(guesses[i]))[0] &&
+                JSON.parse(localStorage.getItem(guesses[i]))[0][0]
                 ? JSON.parse(localStorage.getItem(guesses[i]))[0][0]
                     .similarity === 100
                   ? JSON.parse(localStorage.getItem(guesses[i]))[0][0].word
