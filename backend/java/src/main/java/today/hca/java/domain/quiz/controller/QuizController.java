2187b46a9a3d2349ff9a12a154ae591cb8927a64
andbeyond0331 <mjpark1329@naver.com>
2023-10-03 18:18:00 +0900

    :sparkles: feat: 포기 시 정답 반환 로직 구현

--- backend/java/src/main/java/today/hca/java/domain/quiz/controller/QuizController.java
+++ backend/java/src/main/java/today/hca/java/domain/quiz/controller/QuizController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import today.hca.java.domain.quiz.dto.AnswerResponseDto;
 import today.hca.java.domain.quiz.dto.DailyQuizResponseDto;
-import today.hca.java.domain.quiz.entity.Quiz;
 import today.hca.java.domain.quiz.service.QuizService;
 
 @RestController
@@ -17,10 +18,17 @@ public class QuizController {
 
     @GetMapping("/today")
     public ResponseEntity<?> getQuizByDate(@RequestParam String date) {
-        System.out.println("datae: " + date);
         DailyQuizResponseDto quiz = quizService.getQuizByDate(date);
-        System.out.println("quiz: " + quiz.toString());
-
         return new ResponseEntity( quiz, HttpStatus.OK);
+    }
+
+    @GetMapping("/answer")
+    public ResponseEntity<?> getAnswerForGiveUp(@RequestParam int winState, @RequestParam String date) {
+        AnswerResponseDto quiz;
+        if (winState == 0) {
+            quiz = quizService.getAnswer(winState, date);
+            return new ResponseEntity<>(quiz, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(AnswerResponseDto.builder().message("포기 상태가 아닙니다").build(), HttpStatus.BAD_REQUEST);
     }
 }
